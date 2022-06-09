da7c413ae338da1e78ed133239bb2f8a127cb6ac
Du Motter <cemotterf0@gmail.com>
2022-06-09 14:55:01 -0300

    second commit - lesson 38

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,20 @@
 import './ExpenseItem.css';
 
-function ExpenseItem () {
-    const expenseDate = new Date(2022, 6, 9);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 420.69;
-    
+function ExpenseItem (props) {
+    const month = props.date.toLocaleString('en-US', { month: 'long' });
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+    const year = props.date.getFullYear();
+
     return (
         <div className='expense-item'>
-            <div>{expenseDate.toISOString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
+            </div>
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>${expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
         </div>
     );
