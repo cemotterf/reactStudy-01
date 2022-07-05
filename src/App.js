33cbec177cd4d956a9c0c1cf08eceea1d210fef0
cemotterf <37664804+cemotterf@users.noreply.github.com>
2022-07-04 21:24:27 -0300

    fifth commit - lesson 43

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
@@ -27,9 +27,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <Expenses
-        {...expenses}
-      />
+      <Expenses items={expenses}/>
     </div>
   );
 }
