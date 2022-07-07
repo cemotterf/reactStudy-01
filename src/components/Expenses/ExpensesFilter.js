0db3941aee2e73deb0decac3f1c45be944f790a8
Du Motter <cemotterf0@gmail.com>
2022-07-07 01:42:39 -0300

    ninth commit - task 02 perfected

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,24 +1,15 @@
-import {useState} from 'react';
-
 import './ExpensesFilter.css';
 
 const ExpensesFilter = (props) => {
-
-  const [enteredYear, setEnteredYear] = useState('2022');
-  
-  const yearChangeHandler = (event) => {
-
-    setEnteredYear(event.target.value);
-
+  const dropdownChangeHandler = (event) => {
+    props.onChangeFilter(event.target.value);
   };
-
-  props.onChangeYear(enteredYear);
 
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select value={enteredYear} onChange={yearChangeHandler}>
+        <select value={props.selected} onChange={dropdownChangeHandler}>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
