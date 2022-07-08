fe2c4c956d119f70167a9677a93c47a21a4059e0
Du Motter <cemotterf0@gmail.com>
2022-07-07 23:38:37 -0300

    tenth commit - task 04

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,11 @@
+import { useState } from 'react';
+
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = (props) => {
+
+    const [isEditing, setIsEditing] = useState(false);
 
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
@@ -11,11 +15,27 @@ const NewExpense = (props) => {
         
         props.onAddExpense(expenseData);
 
+        setIsEditing(false);
+
     };
+
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    };
+
+    const stopEditingHandler = () => {
+        setIsEditing(false);
+    }
 
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+            {isEditing && (
+             <ExpenseForm
+              onSaveExpenseData={saveExpenseDataHandler}
+              onCancel={stopEditingHandler}
+             />
+            )}
         </div>
     );    
 };
