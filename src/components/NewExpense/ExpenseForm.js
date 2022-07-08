f54a4138fcb68ba71c466d9a492931c08d3e790f
Du Motter <cemotterf0@gmail.com>
2022-07-08 01:18:32 -0300

    final commit - demo project finished

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -47,7 +47,7 @@ const ExpenseForm = (props) => {
 
         const expenseData = {
             title: enteredTitle,
-            amount: enteredAmount,
+            amount: +enteredAmount,
             date: new Date(enteredDate)
         };
 
